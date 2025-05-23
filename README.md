577dd77b620eabb643913820c49ca9f80bdb9e82
nayuanju <yic153@ucsd.edu>
2025-05-22 22:22:36 -0700

    Fix the problem that picture can't show when click on disable cache in offline

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ blank
 
 Graceful degradation means a website can still work even if some newer features are not supported or if the internet is not working well. Service workers help with this because they let the site save important files (like data, images, or scripts) so it can load even without internet. This makes the app more reliable. Even if the network is slow or offline, users can still see content and use the site like normal.
 
+---
+
 ## Screenshot
 
 ![PWA Installation Screenshot](pwa.png)
--- sw.js
+++ sw.js
@@ -2,6 +2,14 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-8-starter';
+const RECIPE_URLs = [
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json'
+];
 const STATIC_ASSETS = [
   '/',
   '/index.html',
